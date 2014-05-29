43c68234d2b68e4646606255a28ed62942ea44a5
allen2063 <199128927@qq.com>
2014-05-29 23:16:04 +0800

    Initial Commit

---
+++ LeXiang100 Direct Selling/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LeXiang100 Direct Selling
+//
+//  Created by ZengYifei on 14-5-29.
+//  Copyright (c) 2014年 ZengYifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LeXiang100 Direct Selling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LeXiang100 Direct Selling
+//
+//  Created by ZengYifei on 14-5-29.
+//  Copyright (c) 2014年 ZengYifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
